5fdc162b541dedca3b561cc7981abef7c2d8601c
Darkisda <luandesouzanascimento2011@gmail.com>
2020-03-12 06:42:16 -0300

    documentação okay

--- src/FilaCircular.js
+++ src/FilaCircular.js
@@ -1,4 +1,15 @@
+/**
+ * Implementação de um Fila Circular
+ * @author {https://github.com/taniro}
+ */
+
+
 class FilaCircular {
+
+	/**
+	 * Construtor padrão de uma Fila Circular
+	 * @param {number} [t = 10]  - Caso o tamanho da Fila Circular não tenha sido informado
+	 */
 	constructor(t = 10) {
 		this.dados = [];
 		this.fim = -1;
@@ -6,6 +17,11 @@ class FilaCircular {
 		this.tamanho = t;
 	}
 
+	/**
+     * Adiciona um novo elemento em um espaço que esteja vazio na Fila Circular
+     * @param {any} newData - Novo dado a ser colocado na Fila Circular
+     * @throws {Error} - Será lançado um erro caso a Fila Circular esteja cheia
+     */
 	enqueue(newData) {
 		if (this.isFull()) {
 			throw new Error("Queue is full");
@@ -18,6 +34,12 @@ class FilaCircular {
 		this.dados[this.fim] = newData;
 	}
 
+	/**
+     * Remove o dado que esteja na posição da frente da Fila Circular
+	 * Caso a frente seja um outro lugar, a Fila tentará encontrar esse lugar
+     * @returns {any} - Retorna o elemento removido
+     * @throws {Error} - Será lançado um erro caso a Fila Circular esteja vazia
+     */
 	dequeue() {
 		if (this.isEmpty()) {
 			throw new Error("Queue is empty");
@@ -31,14 +53,25 @@ class FilaCircular {
 		return r;
 	}
 
+	/**
+	 * Retorna o elemento que está a frente da Fila
+	 * @returns {any} - Elemento na frente
+	 */
 	front() {
 		return this.dados[this.inicio];
 	}
 
+	/**
+	 * Remove todos os elementos da Fila
+	 */
 	clear() {
 		this.inicio = this.fim = -1;
 	}
 
+	/**
+	 * Retorna a Fila Circular em formatação de String
+	 * @returns {string} - Fila Circular em string 
+	 */
 	toString() {
 		let result = "[";
 		if (this.inicio <= this.fim) {
@@ -57,6 +90,10 @@ class FilaCircular {
 		return result;
 	}
 
+	/**
+	 * Retornará o tamanho da Fila Circular
+	 * @returns {number} - Tamanho da Fila
+	 */
 	size() {
 		if (this.isEmpty()) {
 			return 0;
@@ -69,10 +106,18 @@ class FilaCircular {
 		}
 	}
 
+	/**
+	 * Verifica se a Fila está vazia
+	 * @returns {boolean} - Retorna true se a fila estiver fazia
+	 */
 	isEmpty() {
 		return this.inicio === -1;
 	}
 
+	/**
+	 * Verifica se a Fila está cheia
+	 * @returns {boolean} - Retorna true se a fila estiver cheia
+	 */
 	isFull() {
 		return (
 			(this.inicio === 0 && this.fim === this.tamanho - 1) ||
@@ -80,10 +125,18 @@ class FilaCircular {
 		);
 	}
 
+	/**
+	 * Retorna qual será a próxima posição final da Fila Circular
+	 * @returns {number} - Retorna qual o índice da próxima posição final
+	 */
 	getNextEndPosition() {
 		return this.fim === this.tamanho - 1 && this.inicio !== 0 ? 0 : this.fim + 1;
 	}
 
+	/**
+	 * Retorna qual será a próxima posição inicial da Fila Circular
+	 * @returns {number} - Retorna qual o índice da próxima posição inícial
+	 */
 	getNextBeginPosition() {
 		return this.inicio === this.tamanho - 1 ? 0 : this.inicio + 1;
 	}
